6c049eb7f96a0273e100f3d0c96da06084c4ea63
rawatravinder115 <rawatravinder115@gmail.com>
2021-06-28 19:33:08 +0530

    add UI

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import Card from '../UI/Card'
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
 
@@ -9,13 +10,13 @@ const AddUser = (props) => {
     }
 
     return (
-        <Card>
+        <Card className={classes.input}> 
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text"></input>
             <label htmlFor="age"> Age(Years)</label>
             <input id="age" type="number"></input>
-            <button type="submit"></button>
+            <button type="submit"> Add User</button>
         </form>
         </Card>
     );
